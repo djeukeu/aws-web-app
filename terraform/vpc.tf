94ec06d4e4b2318b833aaf501f3368614ce3a69d
djeukeu <dmxchristian@gmail.com>
2026-09-05 14:09:57 +0100

    fmt

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -50,5 +50,5 @@ resource "aws_route_table_association" "public_subnet_route_association" {
   for_each       = aws_subnet.public_subnet
   subnet_id      = each.value.id
   route_table_id = aws_route_table.public_route_table.id
-  
+
 }
