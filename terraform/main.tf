ed1bf5757f384ec9d0daf3a82440613206103757
djeukeu <dmxchristian@gmail.com>
2026-09-05 14:37:30 +0100

    test ci

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,31 +1,31 @@
-resource "aws_ecr_repository" "ecr_repo" {
-  name                 = "${var.app_name}-repo"
-  image_tag_mutability = "MUTABLE"
-  force_delete         = true
+# resource "aws_ecr_repository" "ecr_repo" {
+#   name                 = "${var.app_name}-repo"
+#   image_tag_mutability = "MUTABLE"
+#   force_delete         = true
 
-  image_scanning_configuration {
-    scan_on_push = true
-  }
+#   image_scanning_configuration {
+#     scan_on_push = true
+#   }
 
-  tags = local.common_tags
+#   tags = local.common_tags
 
-}
+# }
 
-resource "aws_ecs_cluster" "ecs_cluster" {
-  name = "${var.app_name}-cluster"
+# resource "aws_ecs_cluster" "ecs_cluster" {
+#   name = "${var.app_name}-cluster"
 
-  tags = local.common_tags
-}
+#   tags = local.common_tags
+# }
 
-resource "aws_ecs_cluster_capacity_providers" "ecs_capacity_providers" {
-  cluster_name = aws_ecs_cluster.ecs_cluster.name
+# resource "aws_ecs_cluster_capacity_providers" "ecs_capacity_providers" {
+#   cluster_name = aws_ecs_cluster.ecs_cluster.name
 
-  capacity_providers = ["FARGATE"]
+#   capacity_providers = ["FARGATE"]
 
-  default_capacity_provider_strategy {
-    base              = 1
-    weight            = 100
-    capacity_provider = "FARGATE"
-  }
+#   default_capacity_provider_strategy {
+#     base              = 1
+#     weight            = 100
+#     capacity_provider = "FARGATE"
+#   }
 
-}
+# }
